e82c9e7e9d01c11cc5cb02f347664170f2eab600
matthewjparkes <78561107+matthewjparkes@users.noreply.github.com>
2023-04-02 22:18:04 +0100

    messing about

--- index.js
+++ index.js
@@ -4,11 +4,16 @@
 // init project
 var express = require('express');
 var app = express();
+let bodyParser = require('body-parser');
+
+
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
 app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -18,6 +23,9 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/api/:date", function(req, res, next){
+  
+})
 
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
